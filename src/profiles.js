20de998aa372238a6d16f04934af54163d65fd10
parimtm <98881642+parimtm@users.noreply.github.com>
2025-07-17 12:57:28 +0530

    Create profiles.js

---
+++ src/profiles.js
@@ -0,0 +1,25 @@
+const profiles = [
+  {
+    id: '1',
+    name: 'Aanya Sharma',
+    age: 21,
+    bio: 'Loves books and coffee ☕📚',
+    image: 'https://randomuser.me/api/portraits/women/11.jpg'
+  },
+  {
+    id: '2',
+    name: 'Rhea Patel',
+    age: 22,
+    bio: 'Gym rat and dog mom 🐶🏋️‍♀️',
+    image: 'https://randomuser.me/api/portraits/women/12.jpg'
+  },
+  {
+    id: '3',
+    name: 'Ishita Mehta',
+    age: 20,
+    bio: 'Dancer 💃 | Traveller ✈️',
+    image: 'https://randomuser.me/api/portraits/women/13.jpg'
+  }
+];
+
+export default profiles;
